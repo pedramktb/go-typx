c4ed1e913ee45abbe420596b3b5391f37f60a9cc
pedramktb <79080845+pedramktb@users.noreply.github.com>
2026-03-15 17:26:35 +0100

    feat: add Must helpers for error handling and update pointer functions for clarity

---
+++ must.go
@@ -0,0 +1,25 @@
+package typx
+
+// Must is a helper that panics if the error is non-nil, otherwise returning the value.
+func Must[T any](val T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+	return val
+}
+
+// Must2 is a helper that panics if the error is non-nil, otherwise returning the two values.
+func Must2[T, U any](val T, val2 U, err error) (T, U) {
+	if err != nil {
+		panic(err)
+	}
+	return val, val2
+}
+
+// Must3 is a helper that panics if the error is non-nil, otherwise returning the three values.
+func Must3[T, U, V any](val T, val2 U, val3 V, err error) (T, U, V) {
+	if err != nil {
+		panic(err)
+	}
+	return val, val2, val3
+}
--- ptr.go
+++ ptr.go
@@ -2,24 +2,26 @@ package typx
 
 // Ptr creates a pointer to the receiver copy of the given value.
 // Useful for inline pointer creation such as function calls.
+//
+// Deprecated: Since Go 1.26, you can use the built-in new(value) instead.
 func Ptr[T any](value T) *T {
 	return &value
 }
 
 // FromPtr safely dereferences the given pointer and returns it along with a boolean
 // indicating whether the pointer was non-nil.
-func FromPtr[T any](pointer *T) (T, bool) {
-	if pointer == nil {
+func FromPtr[T any](ptr *T) (T, bool) {
+	if ptr == nil {
 		return *new(T), false
 	}
-	return *pointer, true
+	return *ptr, true
 }
 
 // FromPtrOrZero safely dereferences the given pointer and returns its value.
 // If the pointer is nil, it returns the zero value of T.
-func FromPtrOrZero[T any](pointer *T) T {
-	if pointer == nil {
+func FromPtrOrZero[T any](ptr *T) T {
+	if ptr == nil {
 		return *new(T)
 	}
-	return *pointer
+	return *ptr
 }
